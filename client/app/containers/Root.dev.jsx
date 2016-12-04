21b5aff47114ca137b9edd614c374a2f0cfdcd36
mapster <alexander@rosbach.no>
2016-12-04 18:29:24 +0100

    fix lint warnings

--- client/app/containers/Root.dev.jsx
+++ client/app/containers/Root.dev.jsx
@@ -3,10 +3,20 @@ import {Provider} from 'react-redux';
 import App from './App';
 import DevTools from './DevTools';
 
-export default ({store}) =>
-  <Provider store={store}>
-    <div>
-      <App />
-      <DevTools />
-    </div>
-  </Provider>
+export default class RootDev extends React.Component {
+  render() {
+    const {store} = this.props;
+
+    return (
+      <Provider store={store}>
+        <div>
+          <App />
+          <DevTools />
+        </div>
+      </Provider>
+    );
+  }
+}
+RootDev.propTypes = {
+    store: React.PropTypes.object
+};
--- client/app/containers/Root.prod.jsx
+++ client/app/containers/Root.prod.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import App from './App';
 
-export default ({store}) =>
-  <Provider store={store}>
-    <App />
-  </Provider>
+export default class RootDev extends React.Component {
+  render() {
+    const {store} = this.props;
+
+    return (
+      <Provider store={store}>
+          <App />
+      </Provider>
+    );
+  }
+}
+RootDev.propTypes = {
+    store: React.PropTypes.object
+};
