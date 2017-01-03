4dd436c985ac395573b766d761a4ca2b010e4a90
mapster <alexander@rosbach.no>
2017-01-03 21:32:56 +0100

    Switch to class in weightsensor

---
+++ weightsensor/WeightyArduino.js
@@ -0,0 +1,88 @@
+const SerialPort = require("serialport");
+
+module.exports = class WeighthArduino {
+
+  constructor(config) {
+    this.config = config;
+
+    const configError = this.isInvalidConfig(this.config);
+    if (configError) {
+      throw configError;
+    }
+  }
+
+  startListening() {
+    this.connection = new SerialPort(this.config.serialport, {
+      baudRate: 9600,
+      parser: SerialPort.parsers.readline('\n')
+    });
+
+    this.connection.on('open', function(){
+      console.log('Connected to arduino');
+      this.connection.on('data', this.arduinoListener.bind(this));
+    }.bind(this));
+  }
+
+  arduinoListener(rawData) {
+    const trimmedData = rawData.trim();
+    const type = trimmedData.substring(0, trimmedData.indexOf(':'));
+    const data = trimmedData.substring(trimmedData.indexOf(':') + 1).trim();
+
+    switch (type) {
+      case 'msg':
+        this.processMsg(data);
+        break;
+      case 'err':
+        this.processError(data);
+        break;
+      default:
+        this.processData(trimmedData);
+    }
+  }
+
+  processData(data) {
+    const splitAt = data.indexOf(':');
+    const id = Number.parseInt(data.substring(0, splitAt));
+    const value = data.substring(splitAt+1);
+
+    if(Number.isInteger(id)) {
+      console.log(this.config.sensors[id].id + ": " + value);
+    } else {
+      console.log('Unprocessable data received: ' + data);
+    }
+  }
+
+  processMsg(msg) {
+    if (msg == 'ready') {
+      console.log("Arduino ready: sending sensor config");
+      this.sendConfig();
+    } else if (msg.startsWith('Listening')) {
+      console.log("Arduino configured: " + msg);
+    } else {
+      console.error('Unknown message received: ' + msg);
+    }
+  }
+
+  processError(error) {
+    throw 'Error received from arduino: ' + error;
+  }
+
+  sendConfig() {
+    const sensorConfig = this.config.sensors
+    .map(s => '(' + s.outPin + ':' + s.clockPin + ')')
+    .reduce( (a, b) => a + ',' + b);
+
+    this.connection.write(Buffer.from(sensorConfig + '\n'));
+  }
+
+  isInvalidConfig(config) {
+    if (!config.serialport) {
+      return "Invalid config: no serialport specified";
+
+    }
+    if (!config.sensors || !Array.isArray(config.sensors) || config.sensors.length < 1) {
+      return "Invalid config: no sensors specified";
+    }
+    return false;
+  }
+}
