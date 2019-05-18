93c7a17712aa652cff03246c5a7e39bc86b0693a
mapster <alexander@rosbach.no>
2019-05-18 21:59:33 +0200

    add sensor simulator for redis

---
+++ weightsim/redissim.js
@@ -0,0 +1,45 @@
+const redis = require("redis");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const publisher = redis.createClient();
+
+const id = process.argv[2];
+const INITIAL_BASE = 814124;
+
+let base = INITIAL_BASE;
+
+if (!id) {
+  console.log("No weight sensor id given.")
+  process.exit();
+}
+console.log("Simulating weight sensor " + id + " with base " + base);
+
+function generateData() {
+  const value = base + getRandomInt(0, 15);
+  publisher.publish('sensors', JSON.stringify({id, value}))
+}
+
+setInterval(generateData, 1000);
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(cmd) {
+  var arr = cmd.split(" ", 2);
+  if(arr[0] == 'up') {
+    base += parseInt(arr[1]);
+    console.log("Base increased to: " + base);
+  } else if(arr[0] == 'down') {
+    base -= parseInt(arr[1]);
+    console.log("Base decreased to: " + base);
+  } else if(cmd.trim() == 'reset') {
+    base = INITIAL_BASE;
+    console.log("Base reset to: " + base);
+  } else {
+    console.log("Unknown command: " + cmd);
+  }
+});
