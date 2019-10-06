97df84ae149767cf229d032f5c62b5088d5d677f
Alexander Hoem Rosbach <alexander@rosbach.no>
2019-10-06 15:39:31 +0200

    Move editBrew logic into component

--- elm-client/src/Component/EditBrew.elm
+++ elm-client/src/Component/EditBrew.elm
@@ -1,17 +1,21 @@
-module Component.EditBrew exposing (Field(..), Msg(..), update, uploadImage, view)
+module Component.EditBrew exposing (InternalMsg, Model, Msg(..), init, update, updateMutation, uploadImage, view)
 
 import Component.ChooseImage
+import Component.ErrorDetails exposing (ErrorDetails, errorDetails)
 import Component.Form exposing (Field, InputType(..), viewButtons, viewField)
-import Constants exposing (weightyBeerImageUpload)
+import Constants exposing (weightyBeerGraphql, weightyBeerImageUpload)
 import File exposing (File)
+import File.Select as Select
+import Graphql.Http
 import Html exposing (Html, div, form)
 import Html.Attributes exposing (class)
 import Http
 import Maybe.Extra exposing (isJust)
-import Type.Brew exposing (Image, PartialBrew, imageDecoder, isModified, toNewBrew)
+import Type.Brew exposing (Image, PartialBrew, imageDecoder, imageSelection, isModified, makeDeleteImageRequest, toNewBrew)
 import Type.ImageID as ImageID exposing (ImageID)
 import Type.ModifiableValue as Value
 import Utils
+import WeightyBeer.Query as Query
 
 
 type Field
@@ -24,16 +28,123 @@ type Field
 
 
 type Msg
-    = Edit Field String
-    | Save
+    = Save
     | Cancel
+    | Internal InternalMsg
+
+
+type InternalMsg
+    = Edit Field String
     | ToggleGallery
     | RequestImageUpload
+    | UploadImage File
+    | GotUploadedImageId (Result Http.Error Image)
     | DeleteImage ImageID
+    | GotDeletedImageResponse (Result (Graphql.Http.Error ()) (Maybe Image))
+    | GotImagesResponse ImagesResponse
 
 
-update : List Image -> PartialBrew -> Field -> String -> PartialBrew
-update images mutation field value =
+type alias Model =
+    { images : List Image
+    , mutation : PartialBrew
+    , showGallery : Bool
+    , error : Maybe ErrorDetails
+    }
+
+
+type alias ImagesResponse =
+    Result (Graphql.Http.Error ()) (List Image)
+
+
+requestImages : Cmd InternalMsg
+requestImages =
+    Query.images imageSelection
+        |> Graphql.Http.queryRequest weightyBeerGraphql
+        |> Graphql.Http.send (Graphql.Http.discardParsedErrorData >> GotImagesResponse)
+
+
+init : PartialBrew -> ( Model, Cmd InternalMsg )
+init partial =
+    ( Model [] partial False Nothing, requestImages )
+
+
+updateMutation : Model -> PartialBrew -> Model
+updateMutation model partial =
+    { model | mutation = partial }
+
+
+update : InternalMsg -> Model -> ( Model, Cmd InternalMsg )
+update msg model =
+    case msg of
+        Edit field value ->
+            ( { model | mutation = updateMutationField model.images model.mutation field value }, Cmd.none )
+
+        ToggleGallery ->
+            ( { model | showGallery = not model.showGallery }, Cmd.none )
+
+        RequestImageUpload ->
+            ( model, Select.file [ "image/jpeg", "image/png" ] UploadImage )
+
+        UploadImage file ->
+            ( model, uploadImage file )
+
+        GotUploadedImageId result ->
+            updateFromUploadedImage model result
+
+        DeleteImage imageID ->
+            ( model, makeDeleteImageRequest imageID GotDeletedImageResponse )
+
+        GotDeletedImageResponse response ->
+            updateFromDeleteImage model response
+
+        GotImagesResponse imagesResponse ->
+            ( updateImages model imagesResponse, Cmd.none )
+
+
+updateFromDeleteImage : Model -> Result (Graphql.Http.Error ()) (Maybe Image) -> ( Model, Cmd InternalMsg )
+updateFromDeleteImage model response =
+    case response of
+        Ok result ->
+            case result of
+                Just _ ->
+                    ( model, requestImages )
+
+                Nothing ->
+                    ( { model | error = Just (ErrorDetails "Could not delete non-existing image" Nothing) }, Cmd.none )
+
+        Err error ->
+            ( { model | error = Just (errorDetails "Failed to delete image." error) }, Cmd.none )
+
+
+updateFromUploadedImage : Model -> Result Http.Error Image -> ( Model, Cmd InternalMsg )
+updateFromUploadedImage model result =
+    case result of
+        Ok image ->
+            let
+                images =
+                    image :: model.images
+
+                mutation =
+                    updateMutationField images model.mutation Image (ImageID.toString image.id)
+            in
+            ( { model | mutation = mutation, images = images }, requestImages )
+
+        Err _ ->
+            ( { model | error = Just (ErrorDetails "Failed to upload image." Nothing) }, Cmd.none )
+
+
+updateImages : Model -> ImagesResponse -> Model
+updateImages model response =
+    case response of
+        Ok images ->
+            { model | images = images }
+
+        Err error ->
+            { model | error = Just <| errorDetails "Failed to fetch brew images" error }
+
+
+updateMutationField : List Image -> PartialBrew -> Field -> String -> PartialBrew
+updateMutationField images mutation field value =
     case field of
         BrewNumber ->
             { mutation | brewNumber = Value.update mutation.brewNumber (String.toInt value) }
@@ -59,13 +170,13 @@ update images mutation field value =
             }
 
 
-view : List Image -> PartialBrew -> Bool -> Html Msg
-view images partial showGallery =
+view : Model -> Html Msg
+view { images, mutation, showGallery } =
     div [ class "two-column-card" ]
         [ div [ class "column" ]
-            [ viewForm partial ]
+            [ viewForm mutation ]
         , div [ class "column" ]
-            [ viewImage images (Value.toMaybe partial.image) showGallery ]
+            [ viewImage images (Value.toMaybe mutation.image) showGallery ]
         ]
 
 
@@ -73,6 +184,7 @@ viewImage : List Image -> Maybe Image -> Bool -> Html Msg
 viewImage images current showGallery =
     Component.ChooseImage.view images current showGallery
         |> Html.map mapChooseImageMsg
+        |> Html.map Internal
 
 
 viewForm : PartialBrew -> Html Msg
@@ -94,16 +206,16 @@ viewForm partial =
             Field "IBU" (Value.map String.fromInt partial.ibu) True
     in
     form [ class "form" ]
-        [ viewField number Number (Edit BrewNumber)
-        , viewField name Text (Edit Name)
-        , viewField style Text (Edit Style)
-        , viewField abv Number (Edit ABV)
-        , viewField ibu Number (Edit IBU)
+        [ viewField number Number (Edit BrewNumber >> Internal)
+        , viewField name Text (Edit Name >> Internal)
+        , viewField style Text (Edit Style >> Internal)
+        , viewField abv Number (Edit ABV >> Internal)
+        , viewField ibu Number (Edit IBU >> Internal)
         , viewButtons Save Cancel (Utils.and isModified (toNewBrew >> isJust) partial)
         ]
 
 
-mapChooseImageMsg : Component.ChooseImage.Msg -> Msg
+mapChooseImageMsg : Component.ChooseImage.Msg -> InternalMsg
 mapChooseImageMsg msg =
     case msg of
         Component.ChooseImage.ToggleGallery ->
@@ -122,10 +234,10 @@ mapChooseImageMsg msg =
             DeleteImage id
 
 
-uploadImage : (Result Http.Error Image -> msg) -> File -> Cmd msg
-uploadImage msg file =
+uploadImage : File -> Cmd InternalMsg
+uploadImage file =
     Http.post
         { url = weightyBeerImageUpload
         , body = Http.multipartBody [ Http.filePart "brewImage" file ]
-        , expect = Http.expectJson msg imageDecoder
+        , expect = Http.expectJson GotUploadedImageId imageDecoder
         }
--- elm-client/src/Page/NewBrew.elm
+++ elm-client/src/Page/NewBrew.elm
@@ -1,57 +1,31 @@
 module Page.NewBrew exposing (Model, Msg, getError, init, subscriptions, update, view)
 
 import Browser.Navigation as Nav
-import Component.EditBrew exposing (Field)
+import Component.EditBrew
 import Component.ErrorDetails exposing (ErrorDetails, errorDetails)
-import Constants exposing (weightyBeerGraphql)
-import File exposing (File)
-import File.Select as Select
 import Graphql.Http
 import Html exposing (Html, div)
 import Html.Attributes exposing (class)
-import Http
 import Route
-import Type.Brew as Brew exposing (Brew, Image, PartialBrew, emptyPartial, imageSelection, makeDeleteImageRequest, toNewBrew, toPartial)
-import Type.ImageID as ImageID exposing (ImageID)
-import WeightyBeer.Query as Query
+import Type.Brew as Brew exposing (Brew, Image, PartialBrew, emptyPartial, toNewBrew, toPartial)
 
 
 type alias Model =
     { navKey : Nav.Key
-    , mutation : PartialBrew
-    , showGallery : Bool
-    , images : List Image
+    , editModel : Component.EditBrew.Model
     , error : Maybe ErrorDetails
     }
 
 
 type Msg
-    = Edit Field String
-    | Save
+    = Save
     | Cancel
-    | ToggleGallery
-    | GotImagesResponse ImagesResponse
-    | RequestImageUpload
-    | UploadImage File
-    | GotUploadedImageId (Result Http.Error Image)
-    | DeleteImage ImageID
-    | GotDeletedImageResponse (Result (Graphql.Http.Error ()) (Maybe Image))
     | GotSaveResponse SaveResponse
+    | EditBrewMsg Component.EditBrew.InternalMsg
 
 
 type alias SaveResponse =
     Result (Graphql.Http.Error ()) Brew
-
-
-type alias ImagesResponse =
-    Result (Graphql.Http.Error ()) (List Image)
-
-
-requestImages : Cmd Msg
-requestImages =
-    Query.images imageSelection
-        |> Graphql.Http.queryRequest weightyBeerGraphql
-        |> Graphql.Http.send (Graphql.Http.discardParsedErrorData >> GotImagesResponse)
 
 
 getError : Model -> Maybe ErrorDetails
@@ -66,12 +40,16 @@ subscriptions _ =
 
 init : Nav.Key -> ( Model, Cmd Msg )
 init navKey =
-    ( Model navKey emptyPartial False [] Nothing, requestImages )
+    let
+        ( editModel, editCmd ) =
+            Component.EditBrew.init emptyPartial
+    in
+    ( Model navKey editModel Nothing, Cmd.map EditBrewMsg editCmd )
 
 
 makeSaveRequest : Model -> ( Model, Cmd Msg )
 makeSaveRequest model =
-    case toNewBrew model.mutation of
+    case toNewBrew model.editModel.mutation of
         Just brew ->
             ( model, Brew.makeMutationRequest (Brew.createRequest brew Brew.brewSelection) GotSaveResponse )
 
@@ -82,9 +60,6 @@ makeSaveRequest model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Edit field value ->
-            ( { model | mutation = Component.EditBrew.update model.images model.mutation field value }, Cmd.none )
-
         Save ->
             makeSaveRequest model
 
@@ -94,26 +69,12 @@ update msg model =
         Cancel ->
             ( model, navigateToBrews model.navKey )
 
-        ToggleGallery ->
-            ( { model | showGallery = not model.showGallery }, Cmd.none )
-
-        GotImagesResponse imagesResponse ->
-            ( updateImages model imagesResponse, Cmd.none )
-
-        RequestImageUpload ->
-            ( model, Select.file [ "image/jpeg", "image/png" ] UploadImage )
-
-        UploadImage file ->
-            ( model, Component.EditBrew.uploadImage GotUploadedImageId file )
-
-        GotUploadedImageId result ->
-            updateFromUploadedImage model result
-
-        DeleteImage imageID ->
-            ( model, makeDeleteImageRequest imageID GotDeletedImageResponse )
-
-        GotDeletedImageResponse response ->
-            updateFromDeleteImage model response
+        EditBrewMsg editMsg ->
+            let
+                ( editModel, editCmd ) =
+                    Component.EditBrew.update editMsg model.editModel
+            in
+            ( { model | editModel = editModel }, Cmd.map EditBrewMsg editCmd )
 
 
 updateFromSaveResponse : Model -> SaveResponse -> ( Model, Cmd Msg )
@@ -123,49 +84,7 @@ updateFromSaveResponse model response =
             ( { model | error = Just (errorDetails "Failed to save brew" error) }, Cmd.none )
 
         Ok data ->
-            ( { model | mutation = toPartial data }, navigateToBrews model.navKey )
-
-
-updateFromDeleteImage : Model -> Result (Graphql.Http.Error ()) (Maybe Image) -> ( Model, Cmd Msg )
-updateFromDeleteImage model response =
-    case response of
-        Ok result ->
-            case result of
-                Just _ ->
-                    ( model, requestImages )
-
-                Nothing ->
-                    ( { model | error = Just (ErrorDetails "Could not delete non-existing image" Nothing) }, Cmd.none )
-
-        Err error ->
-            ( { model | error = Just (errorDetails "Failed to delete image." error) }, Cmd.none )
-
-
-updateFromUploadedImage : Model -> Result Http.Error Image -> ( Model, Cmd Msg )
-updateFromUploadedImage model result =
-    case result of
-        Ok image ->
-            let
-                images =
-                    image :: model.images
-
-                mutation =
-                    Component.EditBrew.update images model.mutation Component.EditBrew.Image (ImageID.toString image.id)
-            in
-            ( { model | mutation = mutation, images = images }, requestImages )
-
-        Err _ ->
-            ( { model | error = Just (ErrorDetails "Failed to upload image." Nothing) }, Cmd.none )
-
-
-updateImages : Model -> ImagesResponse -> Model
-updateImages model response =
-    case response of
-        Ok images ->
-            { model | images = images }
-
-        Err error ->
-            { model | error = Just <| errorDetails "Failed to fetch brew images" error }
+            ( { model | editModel = Component.EditBrew.updateMutation model.editModel (toPartial data) }, navigateToBrews model.navKey )
 
 
 navigateToBrews : Nav.Key -> Cmd Msg
@@ -176,27 +95,18 @@ navigateToBrews =
 view : Model -> Html Msg
 view model =
     div [ class "single-card-page-container" ]
-        [ Html.map mapEditBrewMsg <| Component.EditBrew.view model.images model.mutation model.showGallery
+        [ Html.map mapEditBrewMsg <| Component.EditBrew.view model.editModel
         ]
 
 
 mapEditBrewMsg : Component.EditBrew.Msg -> Msg
 mapEditBrewMsg msg =
     case msg of
-        Component.EditBrew.Edit field value ->
-            Edit field value
-
         Component.EditBrew.Save ->
             Save
 
         Component.EditBrew.Cancel ->
             Cancel
 
-        Component.EditBrew.ToggleGallery ->
-            ToggleGallery
-
-        Component.EditBrew.RequestImageUpload ->
-            RequestImageUpload
-
-        Component.EditBrew.DeleteImage id ->
-            DeleteImage id
+        Component.EditBrew.Internal internalMsg ->
+            EditBrewMsg internalMsg
