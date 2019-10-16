5f3e01f84946dd2bfdb78993f77e70d95e1c68ab
mapster <alexander@rosbach.no>
2019-10-16 13:26:14 +0200

    Switch to subscription-transport-ws and add simple example

--- elm-client/index.js
+++ elm-client/index.js
@@ -1,7 +1,27 @@
 import { Elm } from './src/Main.elm';
+import { SubscriptionClient } from "subscriptions-transport-ws";
 import './style/style.css';
 
+const gql = `
+subscription {
+  weightUpdated {
+    id
+    zero
+    current
+    percent
+  }
+}
+`;
+
 document.addEventListener('DOMContentLoaded', function () {
+    const client = new SubscriptionClient('ws://localhost:8000/graphql', {reconnect: true});
+
+    client.request({query: gql}).subscribe({
+        next: (value) => console.log('received data: ', value),
+        error: console.log,
+        complete: console.log
+    });
+
     const app = Elm.Main.init({
         node: document.getElementById('elm')
     });
